0f1f56e6478b554ac838be64e3a2b1ad65b0a685
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-11-05 19:32:45 -0700

    build

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
-import requests
+import os
+import sys
+import win32com.shell.shell as shell
+import ctypes
 
-url = ""
-
-bin_id = open('binID.txt', 'r').read()
-url = "https://postb.in/b/" + bin_id
-
-r = requests.get(url)
-print("Get status code: (200=Success)")
-print(r)
-print(r.text)
+def checkAdminStatus():
+    try:
+         return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+if not checkAdminStatus():
+    try:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        string1 = "net user administrator /active:yes"
+        string2 = "runas /user:" + os.getenv('username') + "\Administrator /savecred" + os.path(test.py)
+        os.write(string1)
+        os.write(string2)
+        print ("I am root now.")
+        os.system("pause")
+    except:
+        print("no")
+        os.system("pause")
+        
+else:
+    print ("I am root now.")
+    os.system("pause")
